20551381481f3bdbdee0d8dce1aaecb78cc566f2
Aleksey Dorofeev <aleksei.dorofeev@steppechange.com>
2018-02-07 11:25:10 +0300

    add tests files

--- tests/video_1920x1080.sh
+++ tests/video_1920x1080.sh
@@ -11,5 +11,5 @@ ALG=AES_CM_128_HMAC_SHA1_80
 
 ../.build/srtp_decoder -l -f ${PCAP_FILENAME}.pcap
 ../.build/srtp_decoder -v -f ${PCAP_FILENAME}.pcap -o ${PCAP_FILENAME}_ssrc_${SSRC_A}.opus -s ${SSRC_A} -k ${SSTP_A} -r ${ALG} -c true 2>&1 >${PCAP_FILENAME}_A.log
-../.build/srtp_decoder -v -f ${PCAP_FILENAME}.pcap -o ${PCAP_FILENAME}_ssrc_${SSRC_V}.h264 -s ${SSRC_V} -k ${SSTP_A} -r ${ALG} -c false 2>&1 >${PCAP_FILENAME}_V.log
+../.build/srtp_decoder -v -f ${PCAP_FILENAME}.pcap -o ${PCAP_FILENAME}_ssrc_${SSRC_V}.h264 -s ${SSRC_V} -k ${SSTP_A} -r ${ALG} -c true 2>&1 >${PCAP_FILENAME}_V.log
 
